72f1e68611ae0eaa672d5f7a21ae3472ecbef699
Guido Magni <guibonmagni@gmail.com>
2026-05-18 13:29:23 +0200

    update

--- ex1/ft_garden_data.py
+++ ex1/ft_garden_data.py
@@ -1,5 +1,19 @@
-if __name__ == "__main__":
-	Class Plant:
-	x = 5
+class Plant:
+	def __init__(self, name, height, age):
+		self.name = name
+		self.height = height
+		self.age = age
+	def show(self):
+		print(f"{self.name}: {self.height}cm, {self.age} days old")
 
-	print("=== Welcome to My Garden ===")
+def main():
+	print("=== Garden Plant Registry ===")
+	plant1 = Plant("Rose", 25, 30)
+	plant2 = Plant("Sunflower", 80, 45)
+	plant3 = Plant("Cactus", 15, 120)
+	plant1.show()
+	plant2.show()
+	plant3.show()
+
+if __name__ == "__main__":
+	main()
---
+++ ex3/ft_plant_factory.py
@@ -0,0 +1,30 @@
+class Plant:
+	def __init__(self, name, height, age):
+		self.name = name
+		self.height = height
+		self.age = age
+	def show(self):
+		print(f"{self.name}: {self.height}cm, {self.age} days old")
+	def grow(self):
+		self.height = round(self.height + 0.8, 2)
+	def aging(self):
+		self.age +=1
+
+def main():
+	i = 0
+	print("=== Garden Plant Growth ===")
+	plant1 = Plant("Rose", 25.0, 30)
+	plant2 = Plant("Sunflower", 80, 45)
+	plant3 = Plant("Cactus", 15, 120)
+	while (i <= 7):
+		i = i + 1
+		plant1.show()
+		if i == 8:
+			break
+		print(f"=== Day {i} ===")
+		plant1.grow()
+		plant1.aging()
+	print(f"Growth this week: {round(plant1.height - 25, 2)}cm")
+
+if __name__ == "__main__":
+	main()
